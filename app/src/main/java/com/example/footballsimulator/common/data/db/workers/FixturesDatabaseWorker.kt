316a91a721060845768e0f7e9e714dc092914fbe
Pedro Francisco <>
2022-08-30 16:51:40 +0100

    Fix fixtures generator algorithm

--- app/src/main/java/com/example/footballsimulator/common/data/db/workers/FixturesDatabaseWorker.kt
+++ app/src/main/java/com/example/footballsimulator/common/data/db/workers/FixturesDatabaseWorker.kt
@@ -5,6 +5,7 @@ import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.footballsimulator.common.data.db.dao.FixturesDao
+import com.example.footballsimulator.common.data.db.dao.TeamsDao
 import com.example.footballsimulator.common.data.db.entities.DbFixture
 import com.example.footballsimulator.common.data.db.entities.DbTeam
 import com.example.footballsimulator.common.util.YYYY_MM_DD
@@ -18,94 +19,82 @@ import java.time.Month
 class FixturesDatabaseWorker @AssistedInject constructor(
     @Assisted appContext: Context,
     @Assisted workerParams: WorkerParameters,
-    private val fixturesDao: FixturesDao //TODO replace with repository
+    private val fixturesDao: FixturesDao, //TODO replace with repository
+    private val teamsDao: TeamsDao //TODO replace with repository
 ) : CoroutineWorker(appContext, workerParams) {
 
     override suspend fun doWork(): Result {
         //TODO generate the first batch of fixtures
 
-        val fixtures = generateFixtures(mockTeams)
+
+        val teams = teamsDao.fetchTeams()
+        val randomFourTeams = teams.asSequence()
+            .shuffled()
+            .take(4)
+            .toList()
+
+        val fixtures = generateFixtures(randomFourTeams)
 
         fixturesDao.insertAll(fixtures)
 
         return Result.success()
     }
 
-    private fun generateFixtures(teams: List<DbTeam>): List<DbFixture> {
-        val tournamentStartDate = LocalDate.of(2022, Month.JULY, 30)
-        val numTeams = teams.size
-        val numDays: Int = (numTeams - 1) * 2 // Days needed to complete tournament
+    /**
+     * Berger table algorithm to generate fixtures for all teams home and away
+     */
+    private fun generateFixtures(teamsList: List<DbTeam>): List<DbFixture> {
+        var tournamentStartDate = LocalDate.of(2022, Month.JULY, 30)
+        val numTeams = teamsList.size
+        val numDays: Int = (numTeams - 1) // Days needed to complete tournament
         val halfSize: Int = numTeams / 2
-        val teamsList = mutableListOf<DbTeam>()
+        val teams = mutableListOf<DbTeam>()
         val fixtures = mutableListOf<DbFixture>()
-        teamsList.addAll(teams) // Add teams to List and remove the first team
-        teamsList.removeAt(0)
+        teams.addAll(teamsList) // Add teams to List and remove the first team
+        teams.removeAt(0)
         val teamsSize: Int = teams.size
-        var fixtureDate = tournamentStartDate.plusDays(1)
         for (day in 0 until numDays) {
+            println("Day ${day + 1}")
             val teamIdx = day % teamsSize
-            if (day <= halfSize) {
-                fixtures.add(
-                    DbFixture(
-                        date = fixtureDate.formatToPattern(YYYY_MM_DD),
-                        homeTeamId = teams[teamIdx].teamId,
-                        homeTeamScore = null,
-                        awayTeamId = teams[0].teamId,
-                        awayTeamScore = null,
-                    )
+            fixtures.add(
+                DbFixture(
+                    date = tournamentStartDate.formatToPattern(YYYY_MM_DD),
+                    homeTeamId = teams[teamIdx].teamId,
+                    homeTeamScore = null,
+                    awayTeamId = teamsList[0].teamId,
+                    awayTeamScore = null,
                 )
-            } else {
-                fixtures.add(
-                    DbFixture(
-                        date = fixtureDate.formatToPattern(YYYY_MM_DD),
-                        homeTeamId = teams[0].teamId,
-                        homeTeamScore = null,
-                        awayTeamId = teams[teamIdx].teamId,
-                        awayTeamScore = null,
-                    )
-                )
-            }
+            )
             for (idx in 1 until halfSize) {
                 val firstTeam = (day + idx) % teamsSize
                 val secondTeam = (day + teamsSize - idx) % teamsSize
-                if (day <= halfSize) {
-                    fixtures.add(
-                        DbFixture(
-                            date = fixtureDate.formatToPattern(YYYY_MM_DD),
-                            homeTeamId = teams[firstTeam].teamId,
-                            homeTeamScore = null,
-                            awayTeamId = teams[secondTeam].teamId,
-                            awayTeamScore = null,
-                        )
+                fixtures.add(
+                    DbFixture(
+                        date = tournamentStartDate.formatToPattern(YYYY_MM_DD),
+                        homeTeamId = teams[firstTeam].teamId,
+                        homeTeamScore = null,
+                        awayTeamId = teams[secondTeam].teamId,
+                        awayTeamScore = null,
                     )
-                    println("${teams[firstTeam]} vs ${teams[secondTeam]}")
-                } else {
-                    fixtures.add(
-                        DbFixture(
-                            date = fixtureDate.formatToPattern(YYYY_MM_DD),
-                            homeTeamId = teams[secondTeam].teamId,
-                            homeTeamScore = null,
-                            awayTeamId = teams[firstTeam].teamId,
-                            awayTeamScore = null,
-                        )
-                    )
-                }
+                )
             }
 
-            fixtureDate = fixtureDate.plusDays(1)
+            tournamentStartDate = tournamentStartDate.plusDays(1)
         }
 
-        fixtures.forEach {
-            println("${it.date} ${it.homeTeamId} vs ${it.awayTeamId}")
+        //Now for next round, Rotate team Array
+        val secondRound = fixtures.map {
+            tournamentStartDate = tournamentStartDate.plusDays(1)
+            DbFixture(
+                date = tournamentStartDate.formatToPattern(YYYY_MM_DD),
+                homeTeamId = it.awayTeamId,
+                homeTeamScore = null,
+                awayTeamId = it.homeTeamId,
+                awayTeamScore = null,
+            )
         }
 
+        fixtures.addAll(secondRound)
         return fixtures
     }
 }
-
-private val mockTeams = listOf(
-    DbTeam(name = "Arsenal"),
-    DbTeam(name = "United"),
-    DbTeam(name = "Chelsea"),
-    DbTeam(name = "Leeds"),
-)
